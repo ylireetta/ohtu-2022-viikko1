52b05ddd6bf2ef317c227a021a60961505c4dbf5
Reetta Ylitalo <reetta.k.ylitalo@helsinki.fi>
2022-01-18 18:21:56 +0200

    Viikko 2 tehtävä 2: checkstyle

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -10,8 +10,7 @@ public class Varasto {
     public Varasto(double tilavuus) {  // tilavuus on annettava
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+        } else { // virheellinen, nollataan
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
         saldo = 0;     // oletus: varasto on tyhjä
@@ -20,14 +19,12 @@ public class Varasto {
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+        } else { // virheellinen, nollataan
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
         if (alkuSaldo < 0.0) {
             this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
-        {
+        } else if (alkuSaldo <= tilavuus) { // mahtuu
             this.saldo = alkuSaldo;
         } else {
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -49,22 +46,19 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
-            return;       // tällainen pikapoistuminenkin!
+        if (maara < 0) {
+            return;       // virhetilanteessa voidaan tehdä tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
-            saldo = saldo + maara;          // ihan suoraan sellaisinaan
+        if (maara <= paljonkoMahtuu()) {
+            saldo = saldo + maara;          // omia aksessoreita voi kutsua ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
         }
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
-            return 0.0;   // tällainen pikapoistuminenkin!
+        if (maara < 0) {
+            return 0.0;   // virhetilanteessa voidaan tehdä tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
             double kaikkiMitaVoidaan = saldo;
--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -22,39 +22,5 @@ public class Main {
         System.out.println("Lisätään 50.7");
         mehua.lisaaVarastoon(50.7);
         System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Otetaan 3.14");
-        mehua.otaVarastosta(3.14);
-        System.out.println("Mehuvarasto: " + mehua);
-
-        System.out.println("Virhetilanteita:");
-        System.out.println("new Varasto(-100.0);");
-        Varasto huono = new Varasto(-100.0);
-        System.out.println(huono);
-
-        System.out.println("new Varasto(100.0, -50.7)");
-        huono = new Varasto(100.0, -50.7);
-        System.out.println(huono);
-
-        System.out.println("Olutvarasto: " + olutta);
-        System.out.println("olutta.lisaaVarastoon(1000.0)");
-        olutta.lisaaVarastoon(1000.0);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("mehua.lisaaVarastoon(-666.0)");
-        mehua.lisaaVarastoon(-666.0);
-        System.out.println("Mehuvarasto: " + mehua);
-
-        System.out.println("Olutvarasto: " + olutta);
-        System.out.println("olutta.otaVarastosta(1000.0)");
-        double saatiin = olutta.otaVarastosta(1000.0);
-        System.out.println("saatiin " + saatiin);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("mehua.otaVarastosta(-32.9)");
-        saatiin = mehua.otaVarastosta(-32.9);
-        System.out.println("saatiin " + saatiin);
-        System.out.println("Mehuvarasto: " + mehua);
     }
 }
